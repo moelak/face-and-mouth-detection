8726706b6e063df4ba92d88731608bd7a0e4e09b
moe <moe.lak20@gmail.comgit config --global user.email moe.lak20@gmail.com>
2021-01-17 03:06:36 -0800

    updated the design

--- js/mouthDetection.js
+++ js/mouthDetection.js
@@ -3,101 +3,108 @@
 const video2 = document.querySelector('#video2');
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri(
-    'https://moelak.oudemo.com/_resources/models'
-  ),
-  faceapi.nets.faceLandmark68Net.loadFromUri(
-    'https://moelak.oudemo.com/_resources/models'
-  ),
-  faceapi.nets.faceRecognitionNet.loadFromUri(
-    'https://moelak.oudemo.com/_resources/models'
-  ),
-  faceapi.nets.faceExpressionNet.loadFromUri(
-    'https://moelak.oudemo.com/_resources/models'
-  ),
+	faceapi.nets.tinyFaceDetector.loadFromUri(
+		'https://moelak.oudemo.com/_resources/models'
+	),
+	faceapi.nets.faceLandmark68Net.loadFromUri(
+		'https://moelak.oudemo.com/_resources/models'
+	),
+	faceapi.nets.faceRecognitionNet.loadFromUri(
+		'https://moelak.oudemo.com/_resources/models'
+	),
+	faceapi.nets.faceExpressionNet.loadFromUri(
+		'https://moelak.oudemo.com/_resources/models'
+	),
 ]).then(
-  $('.turnOnCamera2').click(function () {
-    console.log('start');
-    $('.camera-image2').fadeOut();
-    $('.turnOnCamera2').attr('style', 'box-shadow: 1px 3px 12px yellow;');
-    startVideo2();
-  })
+	$('#startBtn2').click(function () {
+		console.log('start');
+		$('.camera-image2').fadeOut();
+		$('#startBtn2').hide();
+		$('#pauseBtn2').show();
+		$('canvas').show();
+		$('.mice-icon').show();
+		startVideo2();
+	})
 );
 
 function startVideo2() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => (video2.srcObject = stream),
-    err => console.error(err)
-  );
+	navigator.getUserMedia(
+		{ video: {} },
+		stream => (video2.srcObject = stream),
+		err => console.error(err)
+	);
 }
 
 video2.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video2);
-  document.querySelector('#mouthDetection').appendChild(canvas);
-  const displaySize = { width: video2.width, height: video2.height };
-  faceapi.matchDimensions(canvas, displaySize);
+	const canvas = faceapi.createCanvasFromMedia(video2);
+	document.querySelector('#mouthDetection').appendChild(canvas);
+	const displaySize = { width: video2.width, height: video2.height };
+	faceapi.matchDimensions(canvas, displaySize);
 
-  $('#startBtn2').click(function () {
-    console.log('click....asf');
-    $('#startBtn2').hide();
-    $('#pauseBtn2').show();
-     $('canvas').show();
-    run2();
-  });
+	run2();
 
-  // setTimeout(run, 2000);
-  var emptyArr = [];
+	$('#pauseBtn2').click(function () {
+		console.log('click....');
+		$('#pauseBtn2').hide();
+		$('#startBtn2').show();
+		$('canvas').hide();
+		$('.mice-icon').hide();
+		clearInterval(interval);
+		exit();
+	});
 
-  function run2() {
-    $('#pauseBtn2').click(function () {
-      console.log('click....');
-      $('#pauseBtn2').hide();
-      $('#startBtn2').show();
-       $('canvas').hide();
-      clearInterval(interval);
-    });
-    var timesRun = 0;
-    emptyArr.length = 0;
-    var interval = setInterval(async () => {
-      const detections = await faceapi
-        .detectAllFaces(video2, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks()
-        .withFaceExpressions();
-      console.log(detections);
+	// setTimeout(run, 2000);
 
-      const resizedDetections = faceapi.resizeResults(detections, displaySize);
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-      faceapi.draw.drawDetections(canvas, resizedDetections);
-      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-      let num =
-        detections[0].landmarks._positions[66]._y -
-        detections[0].landmarks._positions[62]._y;
-      var posNum = num < 0 ? num * -1 : num;
-      console.log('test=> ' + posNum);
+	function run2() {
+		var emptyArr = [];
+		var timesRun = 0;
+		emptyArr.length = 0;
+		console.log(emptyArr.length);
+		var interval = setInterval(async () => {
+			const detections = await faceapi
+				.detectAllFaces(video2, new faceapi.TinyFaceDetectorOptions())
+				.withFaceLandmarks()
+				.withFaceExpressions();
+			console.log(detections);
 
-      emptyArr.push(posNum);
+			const resizedDetections = faceapi.resizeResults(detections, displaySize);
+			canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+			faceapi.draw.drawDetections(canvas, resizedDetections);
+			faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+			faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+			let num =
+				detections[0].landmarks._positions[66]._y -
+				detections[0].landmarks._positions[62]._y;
+			var posNum = num < 0 ? num * -1 : num;
+			console.log('test=> ' + posNum);
 
-      timesRun += 1;
-      console.log('timesRun: ' + timesRun);
-      if (timesRun === 10) {
-        clearInterval(interval);
-        console.log('ARRRRRR: ' + JSON.stringify(emptyArr));
-        var maxNum = JSON.stringify(Math.max.apply(null, emptyArr));
-        console.log(' hahahah-> ' + maxNum);
+			emptyArr.push(posNum);
 
-        if (maxNum > 2.7) {
-          $('.talking').attr('style', 'display: block;');
-          $('.silent').attr('style', 'display: none;');
-        } else {
-          $('.talking').attr('style', 'display: none;');
-          $('.silent').attr('style', 'display: block;');
-        }
-        setTimeout(run2, 100);
-      }
-    }, 100);
-  }
+			timesRun += 1;
+			console.log('timesRun: ' + timesRun);
+			if (timesRun === 10) {
+				clearInterval(interval);
+				console.log('ARRRRRR: ' + JSON.stringify(emptyArr));
+				var maxNum = JSON.stringify(Math.max.apply(null, emptyArr));
+				console.log(' hahahah-> ' + maxNum);
+
+				if (maxNum > 2.7) {
+					$('.talking').attr('style', 'display: block;');
+					$('.silent').attr('style', 'display: none;');
+				} else {
+					$('.talking').attr('style', 'display: none;');
+					$('.silent').attr('style', 'display: block;');
+				}
+
+				if ($('#startBtn2').css('display') == 'none') {
+					setTimeout(run2, 100);
+				} else {
+					$('canvas').remove();
+					return;
+				}
+			}
+		}, 100);
+	}
 });
 
 ////////////// End of the detection section ////////////////
--- js/script.js
+++ js/script.js
@@ -5,35 +5,34 @@ const startBtn = document.querySelector('#startBtn');
 const pauseBtn = document.querySelector('#pauseBtn');
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri(
-    'https://moelak.oudemo.com/_resources/models'
-  ),
-  faceapi.nets.faceLandmark68Net.loadFromUri(
-    'https://moelak.oudemo.com/_resources/models'
-  ),
-  faceapi.nets.faceRecognitionNet.loadFromUri(
-    'https://moelak.oudemo.com/_resources/models'
-  ),
-  faceapi.nets.faceExpressionNet.loadFromUri(
-    'https://moelak.oudemo.com/_resources/models'
-  ),
+	faceapi.nets.tinyFaceDetector.loadFromUri(
+		'https://moelak.oudemo.com/_resources/models'
+	),
+	faceapi.nets.faceLandmark68Net.loadFromUri(
+		'https://moelak.oudemo.com/_resources/models'
+	),
+	faceapi.nets.faceRecognitionNet.loadFromUri(
+		'https://moelak.oudemo.com/_resources/models'
+	),
+	faceapi.nets.faceExpressionNet.loadFromUri(
+		'https://moelak.oudemo.com/_resources/models'
+	),
 ]).then(
-  $('#startBtn').click(function () {
-    console.log('start');
-    $('.camera-image').remove();
-    $('#startBtn').hide();
-    $('#redo').hide()
-    $('#pauseBtn').show();
-    startVideo();
-  })
+	$('#startBtn').click(function () {
+		$('.camera-image').remove();
+		$('#startBtn').hide();
+		$('#redo').hide();
+		$('#pauseBtn').show();
+		startVideo();
+	})
 );
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => (video.srcObject = stream),
-    err => console.error(err)
-  );
+	navigator.getUserMedia(
+		{ video: {} },
+		stream => (video.srcObject = stream),
+		err => console.error(err)
+	);
 }
 
 /*------ End of the detection section-----*/
@@ -41,142 +40,135 @@ var canvas;
 var setVal;
 
 video.addEventListener('play', () => {
-  canvas = faceapi.createCanvasFromMedia(video);
-  document.querySelector('#videoDiv').appendChild(canvas);
-  const displaySize = { width: video.width, height: video.height };
-  faceapi.matchDimensions(canvas, displaySize);
+	canvas = faceapi.createCanvasFromMedia(video);
+	document.querySelector('#videoDiv').appendChild(canvas);
+	const displaySize = { width: video.width, height: video.height };
+	faceapi.matchDimensions(canvas, displaySize);
 
-  run();
+	run();
 
-  pauseBtn.addEventListener('click', function () {
-    $('#pauseBtn').hide();
-    $('#redo').hide();
-    $('#startBtn').show();
-    clearInterval(setVal);
-  });
+	pauseBtn.addEventListener('click', function () {
+		$('#pauseBtn').hide();
+		$('#redo').hide();
+		$('#startBtn').show();
+		clearInterval(setVal);
+	});
 
-  function run() {
-    var counter = 0;
-    var emptyArr = [];
+	function run() {
+		var counter = 0;
+		var emptyArr = [];
 
-    setVal = setInterval(async () => {
-      const detections = await faceapi
-        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks()
-        .withFaceExpressions();
-      console.log(detections);
-      const resizedDetections = faceapi.resizeResults(detections, displaySize);
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-      faceapi.draw.drawDetections(canvas, resizedDetections);
-      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+		setVal = setInterval(async () => {
+			const detections = await faceapi
+				.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+				.withFaceLandmarks()
+				.withFaceExpressions();
+			console.log(detections);
+			const resizedDetections = faceapi.resizeResults(detections, displaySize);
+			canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+			faceapi.draw.drawDetections(canvas, resizedDetections);
+			faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+			faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 
-      var expresion = detections[0].expressions;
-      console.log('expresion:');
-      console.log(
-        Object.keys(expresion).reduce((a, b) =>
-          expresion[a] > expresion[b] ? a : b
-        )
-      );
-      var mostExpresion = Object.keys(expresion).reduce((a, b) =>
-        expresion[a] > expresion[b] ? a : b
-      );
+			var expresion = detections[0].expressions;
+			console.log('expresion:');
+			console.log(
+				Object.keys(expresion).reduce((a, b) =>
+					expresion[a] > expresion[b] ? a : b
+				)
+			);
+			var mostExpresion = Object.keys(expresion).reduce((a, b) =>
+				expresion[a] > expresion[b] ? a : b
+			);
 
-      emptyArr.push(mostExpresion);
+			emptyArr.push(mostExpresion);
 
-      counter += 1;
-      console.log(counter);
+			counter += 1;
+			console.log(counter);
 
-      if (counter === 20) {
-        clearInterval(setVal);
-        $("canvas").hide()
+			if (counter === 20) {
+				clearInterval(setVal);
+				$('canvas').hide();
 
-        console.log('emptyArr: ' + emptyArr);
-        // var arr = JSON.stringify(emptyArr);
+				console.log('emptyArr: ' + emptyArr);
+				// var arr = JSON.stringify(emptyArr);
 
-        let counts = emptyArr.reduce((a, c) => {
-          a[c] = (a[c] || 0) + 1;
-          return a;
-        }, {});
-        let maxCount = Math.max(...Object.values(counts));
-        let mostFrequent = Object.keys(counts).filter(
-          k => counts[k] === maxCount
-        );
+				let counts = emptyArr.reduce((a, c) => {
+					a[c] = (a[c] || 0) + 1;
+					return a;
+				}, {});
+				let maxCount = Math.max(...Object.values(counts));
+				let mostFrequent = Object.keys(counts).filter(
+					k => counts[k] === maxCount
+				);
 
-        console.log('mostFrequent: ' + mostFrequent);
-        // var upper = mostFrequent.charAt(0).toUpperCase() + mostFrequent.substring(1)
-        var span;
-        var something = " ";
-        function moe() {
-          span = $("<span>").attr("class", "test")
-          span.text(" ")
-          something = span.text(mostFrequent);
-        }
-        moe()
+				console.log('mostFrequent: ' + mostFrequent);
+				// var upper = mostFrequent.charAt(0).toUpperCase() + mostFrequent.substring(1)
+				var span;
+				var something = ' ';
+				function moe() {
+					span = $('<span>').attr('class', 'test');
+					span.text(' ');
+					something = span.text(mostFrequent);
+				}
+				moe();
 
-        $('.emotion').append(something)
+				$('.emotion').append(something);
 
+				if ('sad' == mostFrequent || 'angry' == mostFrequent) {
+					// var settings = {
+					// 	async: true,
+					// 	crossDomain: true,
+					// 	url: 'https://dad-jokes.p.rapidapi.com/random/jokes',
+					// 	method: 'GET',
+					// 	headers: {
+					// 		'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
+					// 		'x-rapidapi-key':
+					// 			'a53c3d1b8dmsh1b5a043b6a1b729p12a02cjsncc53fe89ef2d',
+					// 	},
+					// };
 
-        if (('sad' == mostFrequent) || ('angry' == mostFrequent)) {
-          var settings = {
-            async: true,
-            crossDomain: true,
-            url: 'https://dad-jokes.p.rapidapi.com/random/jokes',
-            method: 'GET',
-            headers: {
-              'x-rapidapi-host': 'dad-jokes.p.rapidapi.com',
-              'x-rapidapi-key':
-                'a53c3d1b8dmsh1b5a043b6a1b729p12a02cjsncc53fe89ef2d',
-            },
-          };
+					// $.ajax(settings).then(function (response) {
+					// 	console.log(response);
+					// 	var questions = response.setup;
+					// 	answers = response.punchline
+					// 	$('.question').append(questions);
+					// 	$('.answer').append(answers);
+					// });
+					var sad = `Why aren't koalas actual bears? They don't meet the koalafications.`;
+					$('.question').append(sad);
+					$('.answer').text('');
+				} else if ('happy' == mostFrequent) {
+					var happyTxt = 'Get out of here! ;)';
+					$('.question').append(happyTxt);
+					$('.answer').text('');
+				} else if ('disgusting' == mostFrequent) {
+					var disgustingTxt = "Hey, what's wrong with your face";
+					$('.question').append(disgustingTxt);
+					$('.answer').text('');
+				} else if ('surprised' == mostFrequent) {
+					var surprisedTxt = "Dude, why you're surprised!!!";
+					console.log('surprisedTxt hhhhhhhhhhhh');
+					$('.question').append(surprisedTxt);
+					$('.answer').text('');
+				}
 
-          $.ajax(settings).then(function (response) {
-            console.log(response);
-            var questions = response.setup;
-            // console.log(questions);
-            answers = response.punchline;
-            // console.log(answers);
-
-            $('.question').append(questions);
-            $('.answer').append(answers);
-            /*----- appended the question and answer to the <p> tags in the model section-----*/
-          });
-        }
-        else if ('happy' == mostFrequent) {
-          var happyTxt = "Get out of here! ;)"
-          $('.question').append(happyTxt);
-          $('.answer').text("");
-        }
-        else if ('disgusting' == mostFrequent) {
-          var disgustingTxt = "Hey, what's wrong with your face"
-          $('.question').append(disgustingTxt);
-          $('.answer').text("");
-        }
-        else if ('surprised' == mostFrequent) {
-          var surprisedTxt = "Dude, why you're surprised!!!"
-          console.log("surprisedTxt hhhhhhhhhhhh")
-          $('.question').append(surprisedTxt);
-          $('.answer').text("");
-        }
-
-        $('#startBtn').hide();
-        $('#redo').show()
-        $('#pauseBtn').hide();
-
-
-      }
-    }, 100);
-  }
-  $('#redo').click(function () {
-    console.log('start');
-    $(".question").text(" ")
-    $(".emotion").empty()
-    $('#startBtn').hide();
-    $('#redo').hide()
-    $('#pauseBtn').show();
-    $("canvas").show()
-    run()
-  })
+				$('#startBtn').hide();
+				$('#redo').show();
+				$('#pauseBtn').hide();
+			}
+		}, 100);
+	}
+	$('#redo').click(function () {
+		console.log('start');
+		$('.question').text(' ');
+		$('.emotion').empty();
+		$('#startBtn').hide();
+		$('#redo').hide();
+		$('#pauseBtn').show();
+		$('canvas').show();
+		run();
+	});
 });
 
 ////////////// End of the detection section ///////////////
